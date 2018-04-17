678ad217a9abde348c1184939d532e877843a69b
VFchristoff <6605036@univali.br>
2018-04-17 17:36:04 -0300

    Project updates: new scripts, planet gravity, prefabs

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	//variables
+	public float varGravitation = 1f;
+	public float force;
+	private GameObject planetCore;
+	private GameObject gameSettings;
+
+
+	void Start () {
+
+		planetCore = transform.Find ("Core").gameObject;
+		gameSettings = GameObject.Find ("GameSettings").gameObject;
+	}
+	
+	public void SomethingInOrbit (GameObject bodyInOrbit) {
+		Vector3 distance = planetCore.transform.position - bodyInOrbit.transform.position;
+		Vector3 direction = distance.normalized;
+		float magnitude = distance.sqrMagnitude;
+
+		Rigidbody2D bodyRb = bodyInOrbit.GetComponent<Rigidbody2D> ();
+		float mulletMass = gameSettings.GetComponent<GameSettings> ().mulletMass;
+
+		bodyRb.AddForce (direction * mulletMass * varGravitation / magnitude);
+	}
+
+
+}
---
+++ Assets/Scripts/PlanetOrbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetOrbit : MonoBehaviour {
+
+	private GameObject planet;
+
+	void Start () {
+		planet = this.transform.parent.gameObject;
+	}
+
+	void OnTriggerStay2D (Collider2D coll) {
+		if (coll.gameObject.tag == "Player")
+			planet.GetComponent<Planet> ().SomethingInOrbit (coll.gameObject);
+	}
+
+}
