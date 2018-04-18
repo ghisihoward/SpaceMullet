6308e6231af7aa876c252386f3dd6cf78d0fdc17
Igor Henrique Romer <5810060@univali.br>
2018-04-18 14:36:38 -0300

    First version of Input Manager.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
+
+	Vector2 mousePressed;
+	Vector2 mouseReleased;
+
+	private float mouseDelta = 0f;
+	private float initialTime = 0f;
+	private float resultTime = 0f;
+
 	// Use this for initialization
 	void Start () {
-		
+		mousePressed = new Vector2 ();
+		mouseReleased = new Vector2 ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetMouseButtonDown (0)) {
+			
+			mousePressed = Input.mousePosition;
+			Debug.Log (mousePressed);
+
+			initialTime = Time.time;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			mouseReleased = Input.mousePosition;
+			mouseDelta = mouseReleased.y - mousePressed.y;
+			resultTime = Time.time - initialTime;
+
+			if (mouseDelta > 0) {
+				Debug.Log ("Para cima");
+			}
+		}
 	}
 }
