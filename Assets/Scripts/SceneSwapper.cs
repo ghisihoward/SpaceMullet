8ef8c1a4c2a1af2f4c4153c3aa42b4f5652cf311
Filipe Stefano Ghisi <5848083@univali.br>
2018-04-30 15:41:14 -0300

    Code cleanup, renamed classes to appease The Old Gods.

---
+++ Assets/Scripts/SceneSwapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapper : MonoBehaviour {
+
+	public void changeScene (string scene) {
+		SceneManager.LoadScene (scene);
+	}
+}
