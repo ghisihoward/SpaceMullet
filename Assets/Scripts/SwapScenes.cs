017f777b292af6a2ec2f717797b4869e3b791f57
Igor Henrique Romer <5810060@univali.br>
2018-04-30 15:03:53 -0300

    Menus Scene, Credits and GameOver

---
+++ Assets/Scripts/SwapScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onClickLaunchButton () {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void backToMenu () {
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void creditsButton () {
+		SceneManager.LoadScene ("Credits");
+	}
+}
