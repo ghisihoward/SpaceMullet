7f1da72b68466593c94116cfac3b07bb9f737bba
Filipe Stefano Ghisi <5848083@univali.br>
2018-05-04 17:46:32 -0300

    Sector system vastly improved and made functional.

---
+++ Assets/Scripts/CustomRuntimeFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomRuntimeFunctions : EditorWindow{
+
+	[MenuItem("Window/Custom Runtime Functions")]
+	public static void ShowWindow () {
+		GetWindow<CustomRuntimeFunctions> ("CRF");
+	}
+
+	private void OnGUI () {
+		if (GUILayout.Button ("Spread Sectors Once")) {
+			this.SpreadSectors ();
+		}
+	}
+
+
+	private void SpreadSectors () {
+		GameObject[] sectors = GameObject.FindGameObjectsWithTag ("Sector");
+
+		foreach (GameObject sector in sectors){
+			sector.GetComponent<Sector> ().Spread ();
+		}
+	}
+}
